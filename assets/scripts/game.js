f15a932709b625458efe9087e9ed81140007ff32
Melody-Lisa <>
2023-11-08 15:06:01 +0000

    style:Remove cursor in text area

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -59,6 +59,7 @@ function selectOption(option) {
     state = Object.assign(state, option.setState)
     showTextNode(nextTextNodeId)
 }
+
 const textNodes = [
     {
         id: 1,
@@ -71,7 +72,18 @@ const textNodes = [
         ]
     },
     {
-        id: 2
+        id: 2,
+        text: "You have woken up in your car, it's night time and you had dozed off for a moment. It takes you a moment to get your bearings but you remember that your friend who had been travelling with you had left the car to relieve themself.",
+        options: [
+            {
+                text: "Look around the car",
+                nextText: 3
+            },
+            {
+                text: "Get out to search for your friend",
+                nextText: 4
+            }
+        ]
     }
 ];
 
