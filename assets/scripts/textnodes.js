ce7a4c41db65dc38e1a48a6d7a3a4d9916c20b7c
Melody <137832068+Melody-Lisa@users.noreply.github.com>
2023-11-10 12:41:07 +0000

    docs:"Create textnodes.js and move text nodes across"

---
+++ assets/scripts/textnodes.js
@@ -0,0 +1,26 @@
+const textNodes = [
+    {
+        id: 1,
+        text: "Welcome.. Are you ready to begin?",
+        options: [
+            {
+                text: "Yes",
+                nextText: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: "You have woken up in your car, it's night time and you had dozed off for a moment. It takes you a moment to get your bearings but you remember that your friend who had been travelling with you had left the car to relieve themself.",
+        options: [
+            {
+                text: "Look around the car",
+                nextText: 3
+            },
+            {
+                text: "Get out to search for your friend",
+                nextText: 4
+            }
+        ]
+    }
+];
